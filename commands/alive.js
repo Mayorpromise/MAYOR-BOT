9bf3df9b93d7670f1ded3d0216fdbe3e40931d40
Nwankwo Promise <154396061+Mayorpromise@users.noreply.github.com>
2026-08-03 01:49:06 -0700

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,7 @@
 const settings = require("../settings");
 async function aliveCommand(sock, chatId, message) {
     try {
-        const message1 = `*🤖 LASER MD is Active!*\n\n` +
+        const message1 = `*🤖 MAYOR-BOT is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
@@ -19,7 +19,7 @@ async function aliveCommand(sock, chatId, message) {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363407561123100@newsletter',
-                    newsletterName: 'LASER MD',
+                    newsletterName: 'MAYOR-BOT',
                     serverMessageId: -1
                 }
             }
