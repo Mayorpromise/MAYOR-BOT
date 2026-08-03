84d4c3680489e5decff8f3c338a236f416cdc694
Nwankwo Promise <154396061+Mayorpromise@users.noreply.github.com>
2026-08-03 04:31:43 -0700

    Update settings (2).js

--- settings (2).js
+++ settings (2).js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: '𝐋𝐀𝐒𝐄𝐑 𝐌𝐃',
-  author: 'LRD',
-  botName: "𝐋𝐀𝐒𝐄𝐑 𝐌𝐃",
-  botOwner: '𝙳𝙴𝚅𝚆𝙴𝙴𝙳', // Your name
-  ownerNumber: '50939032060', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'MAYOR-BOT',
+  author: 'MAYOR PROMISE',
+  botName: "MAYOR-BOT",
+  botOwner: 'MAYOR PROMISE', // Your name
+  ownerNumber: '2349072867719', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.9",
-  updateZipUrl: "https://github.com/TECHGOAT333/LASER-MD/archive/refs/heads/main.zip'",
+  updateZipUrl: "https://github.com/Mayorpromise/MAYOR-BOT/archive/refs/heads/main.zip'",
 };
 
 module.exports = settings;
